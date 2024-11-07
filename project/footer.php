b5d2019acf6918b702aa322ea7670e310cae23a1
Sharwari01 <sharwarikrishna2002@gmail.com>
2024-11-07 11:33:37 +0530

    Add files via upload

---
+++ project/footer.php
@@ -0,0 +1,36 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +917338078254 </p>
+         <p> <i class="fas fa-envelope"></i>sheralmascarenhas@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Mangalore, India - 575015 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+      </div>
+
+   </div>
+
+
+</section>
